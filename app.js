995056f2fe395aaf42b21cad1939f6c16bdd5375
MissMuses <chst0001@edu.mercantec.dk>
2023-10-12 11:36:35 +0200

    Aflevering
    Aflevering 12/10

--- app.js
+++ app.js
@@ -3,24 +3,20 @@
 
 
     let Bears =0;
+    let UpBears =0;
 
     function myBear(){
-        Bears += 1;
+        Bears += (1+UpBears);
         document.getElementById("TheBears").innerHTML = Bears;
-        
-    
+        document.getElementById("Counter").innerHTML = Bears; 
     }
 
-    //ShowCounter
-    function ShowBears(){
-    document.getElementById("Counter").innerHTML = Bears;
-    }
 
 //Multiplier 
 
 function multiplier(){
-if (Bears >=10) {
-    Bears +=5;
+if (Bears >=10 && UpBears===0) { //Når brugt giver den ikke lægnere 0 og stopper med at virke. 
+    UpBears +=4;
     Bears -=10;
     document.getElementById("TheBears").innerHTML = Bears; 
 }
